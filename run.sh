d66844db0f5ce86ed1c16f7b36002841dc44a711
Brian Wakenshaw <36340167+BriWak@users.noreply.github.com>
2019-03-22 10:41:30 +0000

    Add functionality to accept all sky remote commands

--- run.sh
+++ run.sh
@@ -3,20 +3,23 @@ set -e
 
 SKY_IP=$(jq --raw-output ".sky_ip" options.json)
 
-# Read from STDIN aliases to send shutdown
+commands="sky power tvguide home boxoffice services search interactive sidebar up down left right select channelup channeldown i backup dismiss text help play pause rewind fastforward stop record red green yellow blue 0 1 2 3 4 5 6 7 8 9"
+
+# Read from STDIN aliases to send command
 while read -r input; do
+    
     # remove json stuff
     input="$(echo "$input" | jq --raw-output '.')"
     echo "[Info] Read input: $input"
 
-    # Check for the correct command
-    if [ "$input" != "power" ]; then
-		# Not the correct command
-        continue
-    else
-        # The correct command
-        eval sky-remote-cli "$SKY_IP" power
-        echo "[Info] power command sent"
-    fi
-
+        # Check for a valid command
+        if [[ ! "$commands" =~ " $input " ]]; then
+            # Not a valid command
+            echo "[Warn] $input is not a valid command"
+            continue
+        else
+            # Valid command
+            eval sky-remote-cli "$SKY_IP" "$input"
+            echo "[Info] $input command sent"
+        fi
 done
